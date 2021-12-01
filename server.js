1ba90390c5aeae5ea84119d4ca967d7fa2cfe145
jani358 <janindugayanga10@gmail.com>
2021-12-01 21:34:09 +0530

    deplying to heroku

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/WEBPRO'));
+app.use(express.static(__dirname + '/dist/material-demo'));
  
 app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
+  // Replace the '/dist/material-demo/index.html'
   res.sendFile(path.join(__dirname + '/dist/WEBPRO/index.html'));
 });
 // Start the app by listening on the default Heroku port
